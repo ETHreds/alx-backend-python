4b9ccdc209d8c49a869c40e3efd958aaf053b77d
ETHreds <jcmaglaw@gmail.com>
2023-10-06 00:30:14 +0300

    Concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/ env python3
+"""
+function concat that takes a string str1 and a string str2 as
+arguments and returns a concatenated string"""
+
+
+def concat(string1: str, string2: str) -> str:
+    """ Combine two strings no space"""
+    return string1 + string2
