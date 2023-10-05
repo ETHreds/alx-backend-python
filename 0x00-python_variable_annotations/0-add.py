9cf4466c0e1a56f9dc58101b7dc322f4d83e0219
ETHreds <jcmaglaw@gmail.com>
2023-10-05 23:56:23 +0300

    Add tru doc string

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+"""This is the docstring of the 0-add module.
+This module provides a function for adding two numbers.
+"""
 
 def add(a:float, b:float) -> float:
-    """Function to add"""
+    """Add two numbers and return the result."""
     return a + b
