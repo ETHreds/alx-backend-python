742e37fca5f2d768488e35437f83b0c29552414d
ETHreds <jcmaglaw@gmail.com>
2023-10-06 12:14:58 +0300

    float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""function floor which takes a float n as
+argument and returns a string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Args:n (float): The input float.
+    Returns:int: The floor of the input float as a string."""
+    return str(n)
