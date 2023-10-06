803d136008c5f5beb5724db7d2b3a5977998ab3e
ETHreds <jcmaglaw@gmail.com>
2023-10-06 12:46:37 +0300

    remove trailling space

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -6,6 +6,6 @@ Function sum_list that sums up the floats in a list"""
 
 def sum_list(input_list: List[float]) -> float:
     """
-    Arg- List of floats 
+    Arg- List of floats
     return - sum of floats in list as float"""
     return sum(input_list)
