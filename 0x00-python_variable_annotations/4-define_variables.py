5e27a5d1915c36725991a686756e8d8ac3949836
ETHreds <jcmaglaw@gmail.com>
2023-10-07 19:57:51 +0300

    Ducky

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -3,7 +3,19 @@
 Annotated variables
 """
 
-a: int = 1
-pi: float = 3.14
-i_understand_annotations: bool = True
-school: str = "Holberton"
+from typing import Iterable, Sequence, List, Tuple
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Calculate the length of each element in the input iterable and return the results as a list of tuples.
+
+    Args:
+        lst (Iterable[Sequence]): An iterable containing sequences (e.g., strings, lists, tuples).
+
+    Returns:
+        List[Tuple[Sequence, int]]: A list of tuples, where each tuple contains two elements:
+        - The first element is a sequence from the input iterable.
+        - The second element is an integer representing the length of the sequence.
+
+    """
+    return [(i, len(i)) for i in lst]
