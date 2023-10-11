8bfcaadc56baf7bc6bbdc72e73d317f87ec9d3ce
ETHreds <jcmaglaw@gmail.com>
2023-10-11 23:41:27 +0300

    Unpacking operator

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -14,11 +14,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Get time of operation"""
     start_time = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+
     end_time = time.time()
     return end_time - start_time
