21b5bf3ad293bc5ba45e4f2f6ff4aee15d9dabda
ETHreds <jcmaglaw@gmail.com>
2023-10-11 18:30:14 +0300

    return  iritable

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+async_comprehension
+"""
+
+
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    result = [i async for i in async_generator()]
+    return result
