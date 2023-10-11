f2b4f253c282e3d82d9f204e29c12233a8d198f9
ETHreds <jcmaglaw@gmail.com>
2023-10-11 23:32:38 +0300

    function documentation

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -12,6 +12,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """Get time of operation"""
     start_time = time.time()
     await asyncio.gather(
         async_comprehension(),
